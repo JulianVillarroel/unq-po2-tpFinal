61daf8fe451752e24c0767d534b7f015f7cadb15
Julián <Julin@DESKTOP-HT13SH7>
2023-05-20 13:27:42 -0300

    estructura basica del proyecto

---
+++ test/ar/edu/unq/po2/tpFinal/Test.java
@@ -0,0 +1,12 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
