16577a7fd17b95d1541c6eb5c807742bab5404c9
Julián <Julin@DESKTOP-HT13SH7>
2023-05-29 15:50:40 -0300

    Objetos necesarios, para crear las carpetas y dar un orden

---
+++ src/ar/edu/unq/po2/tpFinal/EstadoDeMuestra/EstadoDeMuestraVerificada.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.tpFinal.EstadoDeMuestra;
+
+public class EstadoDeMuestraVerificada implements EstadoDeMuestra {
+
+}
---
+++ src/ar/edu/unq/po2/tpFinal/EstadoDeUsuario/EstadoDeUsuario.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.tpFinal.EstadoDeUsuario;
+
+public abstract class EstadoDeUsuario {
+
+}
