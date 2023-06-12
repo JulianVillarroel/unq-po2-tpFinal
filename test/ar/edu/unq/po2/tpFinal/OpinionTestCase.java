71eb3c2b1eae223715da4adb63b584f8bb0925dc
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2023-06-12 11:44:27 -0300

    cree todos los test

---
+++ test/ar/edu/unq/po2/tpFinal/OpinionTestCase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OpinionTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
