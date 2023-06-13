43f9ec02785b0e5cda7089f04ae632c0a48a8235
Julián <Julin@DESKTOP-HT13SH7>
2023-06-13 17:43:30 -0300

    test de opinion

--- test/ar/edu/unq/po2/tpFinal/OpinionTestCase.java
+++ test/ar/edu/unq/po2/tpFinal/OpinionTestCase.java
@@ -2,13 +2,41 @@ package ar.edu.unq.po2.tpFinal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tpFinal.Enumerativos.Calificacion;
 
 class OpinionTestCase {
 
+	Opinion opinion;
+	Calificacion calificacionDeOpinion;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		calificacionDeOpinion = Calificacion.CHINCHE_FOLIADA;
+		opinion = new Opinion(calificacionDeOpinion);
+
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testOpinionCalificacionDeChincheFoliada() {
+		assertEquals(opinion.getCalificacion(), calificacionDeOpinion);
+	}
+
+	@Test
+	void testOpinionCalificacionImagenPocoClara() {
+		calificacionDeOpinion = Calificacion.IMAGEN_POCO_CLARA;
+		opinion.setCalificacion(calificacionDeOpinion);
+		assertEquals(opinion.getCalificacion(), calificacionDeOpinion);
+	}
+
+	@Test
+	void testFechaEnQueSeEmiteLaOpinion() {
+		LocalDate fechaEmision = LocalDate.now();
+		assertEquals(opinion.getFechaDeEmision(), fechaEmision);
 	}
 
 }
